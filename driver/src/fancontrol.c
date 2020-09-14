0da1ab70fdfaf6136aeb9f73fa3f89713119eee8
Dimitar Atanasov <datanasov@cpdbg.com>
2020-09-14 13:08:20 +0300

    *fix maxrpm for Blade 2020 advanced;
    Signed-off-by: Dimitar Atanasov <datanasov@cpdbg.com>

--- driver/src/fancontrol.c
+++ driver/src/fancontrol.c
@@ -21,6 +21,7 @@ static int get_max_fan_rpm(__u32 product_id)
 	case BLADE_2019_MERC:
 	case BLADE_PRO_2019:
 	case BLADE_2019_STEALTH:
+    case BLADE_2020_ADV:
 		return MAX_FAN_RPM_STEALTH;
 	default:
 		return MAX_FAN_RPM_DEFAULT;
