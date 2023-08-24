6d3cd51738141addb7cc50ef6e854cbc5418bda7
haden <git@haden.io>
2023-08-23 18:06:35 -0700

    add more error checking and verbose messages

--- razer_control_gui/install.sh
+++ razer_control_gui/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Build the project
+echo "Building the project..."
 cargo build --release
 
 # Check if the build was successful
@@ -8,7 +10,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo "Stopping razerdaemon service..."
 systemctl --user stop razerdaemon.service
+
+echo "Creating directories, copying files, and setting up services..."
 mkdir -p ~/.local/share/razercontrol
 sudo /bin/bash << EOF
 mkdir -p /usr/share/razercontrol
@@ -20,6 +25,23 @@ cp data/udev/99-hidraw-permissions.rules /etc/udev/rules.d/
 cp razerdaemon.service /usr/lib/systemd/user/
 udevadm control --reload-rules
 EOF
+
+# Check if the previous commands were successful
+if [ $? -ne 0 ]; then
+    echo "An error occurred while setting up, exiting."
+    exit 1
+fi
+
+echo "Enabling razerdaemon service..."
 systemctl --user enable razerdaemon.service
+
+echo "Starting razerdaemon service..."
 systemctl --user start razerdaemon.service
+
+# Check if the service started successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to start razerdaemon service, exiting."
+    exit 1
+fi
+
 echo "Install complete!"
