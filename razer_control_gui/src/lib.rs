79b4949a1a173de59e1458ef1fcd9dd2c5d9c7d9
Josu Goñi <josu_z@hotmail.com>
2024-10-19 21:00:57 +0200

    Add lib.rs to share things
    Signed-off-by: Dimitar Atanasov <datanasov@cpdbg.com>

---
+++ razer_control_gui/src/lib.rs
@@ -0,0 +1,14 @@
+//! This is duplicated stuff for now, until we have a proper project structure
+
+use serde::{Serialize, Deserialize};
+
+pub const DEVICE_FILE: &str = "/usr/share/razercontrol/laptops.json";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct SupportedDevice {
+    pub name: String,
+    pub vid: String,
+    pub pid: String,
+    pub features: Vec<String>,
+    pub fan: Vec<u16>,
+}
