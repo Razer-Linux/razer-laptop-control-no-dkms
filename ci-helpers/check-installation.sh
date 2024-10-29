855c3950c06ad5b96950fd0be1cc2d2b3c2b59db
Josu Goñi <josu_z@hotmail.com>
2024-10-29 13:25:01 +0100

    Improve CI to check the install script
    Signed-off-by: Dimitar Atanasov <datanasov@cpdbg.com>

---
+++ ci-helpers/check-installation.sh
@@ -0,0 +1,29 @@
+set -o errexit
+set -o nounset
+set -o pipefail
+
+check_file() {
+    if [ ! -e $1 ]; then 
+        echo "File \"$1\" does not exist."
+        exit 1
+    fi
+}
+
+echo "Checking the existence of the necessary files..."
+# Daemon
+# check_file "~/.local/share/razercontrol"
+check_file "/usr/share/razercontrol/daemon"
+check_file "/usr/share/razercontrol/laptops.json"
+check_file "/etc/udev/rules.d/99-hidraw-permissions.rules"
+# CLI
+check_file "/usr/bin/razer-cli"
+# GUI
+check_file "/usr/bin/razer-settings"
+check_file "/usr/share/applications/razer-settings.desktop"
+echo "All files are present"
+
+
+printf "Checking that the service is enabled: "
+systemctl --user is-enabled razercontrol.service
+
+echo "Done!"
